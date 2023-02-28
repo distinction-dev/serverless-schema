afca5c7abcc631c0994f00a4ac5a3672725d39e2
KillDozerX2 <himanshupant127@gmail.com>
2023-02-28 19:00:47 +0530

    fix: :memo: added docs, to get a new fix release as semantic release caused bugs

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -12,6 +12,9 @@ export declare type DeepReadonly<T> = T extends O.Object
 
 /**
  * Will make all the property present in the object to be non readonly (mutable).
+ * So if you have a type like this:- `{ readonly prop: string }`, this will change it to
+ * `{ prop: string }`.
+ * This is useful when you're doing `as const` for better type inference
  */
 export type Mutable<T> = {
   -readonly [P in keyof T]: T[P];
